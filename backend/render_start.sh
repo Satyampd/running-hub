d7c9fb7bbc57fad054e8104d008198f59d4e58c0
Satyam Prasad Tiwari <Satyamprasadtiwari@gmail.com>
2025-05-20 00:52:01 +0530

    Update render_start.sh

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -5,6 +5,10 @@ set -e
 echo "Setting up database..."
 python app/scripts/create_db.py
 
+# Scraping the data
+echo "Scraping the data.."
+python -m app.scripts.scrape_cli > scraper.log 2>&1 &
+
 # Start the FastAPI application
 echo "Starting FastAPI application..."
-exec uvicorn app.main:app --host 0.0.0.0 --port $PORT 
+exec uvicorn app.main:app --host 0.0.0.0 --port $PORT
